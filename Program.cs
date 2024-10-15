9997c5ee57f40f629b670dd2240c596c66d9030f
GustavFasa <150124886+GustavFasa@users.noreply.github.com>
2024-10-15 10:20:19 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace Lab2_Grupp2
         static void Main(string[] args)
         {
             console.writeline("hej vi testar igen");
+            Console.WriteLine("Hejhej");
         }
     }
 }
